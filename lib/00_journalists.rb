8e627d5dd8a092b7eb1e77356bacc500aa57089a
DamiC0de <dreymidot@gmail.com>
2023-04-13 11:43:53 +0200

    Small script allowing from an array to draw handles by size, alphabetical order etc. All grouped in a menu from 1..8, 0 to exit the loop

---
+++ lib/00_journalists.rb
@@ -0,0 +1,114 @@
+require_relative "001_AINSI.rb"
+
+
+twitter = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+
+
+def effacer_console
+    if Gem.win_platform?
+      system("cls")
+    else
+      system("clear")
+    end
+  end
+
+
+loop do
+    puts ""
+    puts ""
+    puts "BIG DATA:"
+    puts "1. Combien y a-t-il de handle dans cette array ?"
+    puts "2. Quel est le handle le plus court de cette liste ?"
+    puts "3. Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)"
+    puts "4. Combien commencent par une majuscule (première lettre juste après le @) ?"
+    puts "5. Trie la liste de handle par ordre alphabétique."
+    puts "6. Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)"
+    puts "7. Quelle est la position dans l'array de la personne @epenser ?"
+    puts "8. Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)"
+    puts "0. Quitter"
+  
+    print "Entrez votre choix (1-8) ou 0 pour quitter: "
+    choix = gets.chomp.to_i
+
+
+    case choix
+    when 1
+        effacer_console
+      # 1. Combien y a-t-il de handle dans cette array ?
+        puts yellow_bold ("Le tableau est composé de #{twitter.length} élements !")
+
+    when 2
+      # 2. Quel est le handle le plus court de cette liste ?
+        effacer_console
+        handle_shorter  = twitter.min_by {|handle| handle.length }
+        puts yellow_bold ("L'élement le plus court est: #{handle_shorter}.")
+
+    when 3
+      # 3. Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)
+        # ici on utilise select -> parcours l'ensemble du tableau et ne conserve que ceux dont la longueur = 6 (5 caracteres sans le @)
+        # la méthode count compte le nombre d'éléments dans le nouvel array
+        effacer_console
+        number_handles_5_caracteres     = twitter.select { |handle| handle.length == 6 }.count
+        puts yellow_bold ("Il y a: #{number_handles_5_caracteres} éléments composés de 5 caractères.")
+
+      
+    when 4
+      # 4. Combien commencent par une majuscule (première lettre juste après le @) ?
+        # on commence a compter a partir de l'indice 1 poiur exclure le @
+        effacer_console
+        number_handles_uppercase = twitter.select { |handle| handle[1].match?(/[A-Z]/) }.count
+        puts yellow_bold ("Il y a: #{number_handles_uppercase} éléments qui commencent par une majuscule.")
+
+      
+    when 5
+      # 5. Trie la liste de handle par ordre alphabétique.
+        # sort trie les éléments de l'array twitter par ordre alphabétique
+        effacer_console
+        twitter_sort = twitter.sort
+        puts yellow_bold ("Voici votre tableau trié par ordre alphabétique:")
+        puts ""
+        puts twitter_sort
+
+      
+    when 6
+      # 6. Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)*
+        effacer_console
+        twitter_sort_by_length = twitter.sort_by { |handle| handle.length }
+        puts yellow_bold ("Voici les éléments de votre tableau trié par longueur, les plus petits avant, les plus grands en dernier:")
+        puts ""
+        puts twitter_sort_by_length
+
+      
+    when 7
+      # 7. Quelle est la position dans l'array de la personne @epenser ?
+        effacer_console
+        position_epenser = twitter.index("@epenser")
+        puts yellow_bold ("L'élement @epenser se trouve à la #{position_epenser} ème position dans le tableau")
+      
+    when 8
+      # 8. Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)
+        # ici on utilise group_by => regroupe les éléments en fonction de leurs tailles sans compter le @ (length - 1)
+        # on utilise ensuite transform_values => remplace les valeurs par leur nombre d'élements respectifs en utilisant count
+        repartition_by_size = twitter.group_by { |handle| handle.length - 1 }.transform_values(&:count)
+
+        # ici on utilise sort_by en premier afin de trier les handles en fonction de leurs tailles
+        # puis on utilise each pour voir ce qu'il y a dans ce tableau trié par taille et renvoyer le résultat demandé
+        # length => clef (taille de caractères) && _ => valeur (nombre d'éléments pour chacune de ces tailles)
+        effacer_console
+        repartition_by_size.sort_by{ |length, _| length}.each do |length, number|
+            puts yellow_bold ("Il y a #{number} éléments composés de #{length} caractères")
+        end
+
+    when 0
+        effacer_console
+        puts yellow_bold ("Allez ciao !")
+        puts ""
+        break
+    else
+        puts ""
+      puts red_bold("Choix invalide. Veuillez choisir une option entre 1 et 9, ou 0 pour quitter.")
+    end
+  
+    puts "\n"
+  end
